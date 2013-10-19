75441077b42321da3b366de0ea36e940e1b839c0
Atsushi Mizumoto <atsushi@mizumot.com>
2013-10-19 23:33:35 +0900

    Update server.R

--- server.R
+++ server.R
@@ -8,22 +8,27 @@ shinyServer(function(input, output) {
   bs <- reactive({
     x <- input$textarea.in
     x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
-    return(describe(x))
+    result <- describe(x)[2:13]
+    row.names(result) <- ""
+    return(result)
     })
 
    output$distPlot <- renderPlot({
     x <- input$textarea.in
     x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
     hist(x, breaks="FD",main="", xlab="", col = "cyan")
-    abline(v = mean(x), col = "red", lwd = 2)
+    rug(x)
+    abline(v = mean(x, na.rm=T), col = "red", lwd = 2)
     })
 
    output$boxPlot <- renderPlot({
     x <- input$textarea.in
     x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
-    boxplot(x, horizontal=TRUE)
-    stripchart(x, pch = 16, horizontal=TRUE, add = TRUE)
-    abline(v = mean(x), col = "red", lwd = 2)
+    boxplot(x, horizontal=TRUE, xlab= "Mean and +/-1 SD are displayed in red.")
+    stripchart(x, pch = 16, add = TRUE)
+    points(mean(x, na.rm=T), 0.9, pch = 18, col = "red", cex = 2)
+    arrows(mean(x, na.rm=T), 0.9, mean(x, na.rm=T) + sd(x, na.rm=T), length = 0.1, angle = 45, col = "red")
+    arrows(mean(x, na.rm=T), 0.9, mean(x, na.rm=T) - sd(x, na.rm=T), length = 0.1, angle = 45, col = "red")
     })
 
    testnorm <- reactive({
