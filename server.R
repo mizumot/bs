7078fb0508ed60ac3d66ac13ff330cd081ee27b4
Atsushi Mizumoto <atsushi@mizumot.com>
2015-08-13 00:20:03 +0900

    synced
    synced

--- server.R
+++ server.R
@@ -1,156 +1,44 @@
 library(shiny)
-library(psych)
+library(shinyAce)
+library(CTT)
+
+
 
 shinyServer(function(input, output) {
     
-    options(warn=-1)
     
-    bs <- reactive({
-        x <- input$textarea.in
-        x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
-        result <- describe(x)[2:13]
-        row.names(result) <- ""
-        return(result)
-    })
+    check <- reactive({
+        if (input$colname == 0) {
+            x <- read.table(text=input$text, sep="", na.strings=c("","NA","."))
+            x <- as.matrix(x)
+            
+            ans <- read.delim(text=input$anskey, sep="", fill=TRUE, header=FALSE, stringsAsFactors=FALSE)
+            ans <- as.character(ans)
+            dat <- score(x, ans, output.scored=TRUE)$scored
+            
+        } else {
+            x <- read.table(text=input$text, header = TRUE, sep="", na.strings=c("","NA","."))
 
-
-
-    makedistPlot <- function(){
-        x <- input$textarea.in
-        x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
-        
-        x <- x[!is.na(x)]
-        
-        simple.bincount <- function(x, breaks) {
-            nx <- length(x)
-            nbreaks <- length(breaks)
-            counts <- integer(nbreaks - 1)
-            for (i in 1:nx) {
-                lo <- 1
-                hi <- nbreaks
-                if (breaks[lo] <= x[i] && x[i] <= breaks[hi]) {
-                    while (hi - lo >= 2) {
-                        new <- (hi + lo) %/% 2
-                        if(x[i] > breaks[new])
-                        lo <- new
-                        else
-                        hi <- new
-                    }
-                    counts[lo] <- counts[lo] + 1
-                }
-            }
-            return(counts)
+            ans <- read.delim(text=input$anskey, sep="", fill=TRUE, header=FALSE, stringsAsFactors=FALSE)
+            ans <- as.character(ans)
+            
+            dat <- score(x, ans, output.scored=TRUE)$scored
         }
-        
-        nclass <- nclass.FD(x)
-        breaks <- pretty(x, nclass)
-        counts <- simple.bincount(x, breaks)
-        counts.max <- max(counts)
-        
-        h <- hist(x, las=1, breaks="FD", xlab= "Red vertical line shows the mean.",
-        ylim=c(0, counts.max*1.2), main="", col = "cyan")
-        rug(x)
-        abline(v = mean(x, na.rm=T), col = "red", lwd = 2)
-        xfit <- seq(min(x, na.rm=T), max(x, na.rm=T))
-        yfit <- dnorm(xfit, mean = mean(x, na.rm=T), sd = sd(x, na.rm=T))
-        yfit <- yfit * diff(h$mids[1:2]) * length(x)
-        lines(xfit, yfit, col = "blue", lwd = 2)
-    }
+    })
+    
+    
 
+    output$check <- renderTable({
+        head(check(), n = 10)
+    }, digits = 0)
 
-    output$distPlot <- renderPlot({
-        print(makedistPlot()) # 上の function を参照する指定
-    })
-    
-    
-    
-    makeboxPlot <- function(){
-        x <- input$textarea.in
-        x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
-        boxplot(x, horizontal=TRUE, xlab= "Mean and +/-1 SD are displayed in red.")
-        stripchart(x, pch = 16, add = TRUE)
-        points(mean(x, na.rm=T), 0.9, pch = 18, col = "red", cex = 2)
-        arrows(mean(x, na.rm=T), 0.9, mean(x, na.rm=T) + sd(x, na.rm=T), length = 0.1, angle = 45, col = "red")
-        arrows(mean(x, na.rm=T), 0.9, mean(x, na.rm=T) - sd(x, na.rm=T), length = 0.1, angle = 45, col = "red")
-    }
-    
-    
-    output$boxPlot <- renderPlot({
-        print(makeboxPlot()) # 上の function を参照する指定
-    })
-    
-    
-    
-    testnorm <- reactive({
-        x <- input$textarea.in
-        x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
-        list(ks.test(scale(x), "pnorm"), shapiro.test(x))
-    })
-    
-    
-    
-    makeqqPlot <- function(){
-        x <- input$textarea.in
-        x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
-        qqnorm(x, las=1)
-        qqline(x, col=2)
-    }
-    
-    output$qqPlot <- renderPlot({
-        print(makeqqPlot()) # 上の function を参照する指定
-     })
-    
-    
-    
-    info <- reactive({
-        info1 <- paste("This analysis was conducted with ", strsplit(R.version$version.string, " \\(")[[1]][1], ".", sep = "")# バージョン情報
-        info2 <- paste("It was executed on ", date(), ".", sep = "")# 実行日時
-        cat(sprintf(info1), "\n")
-        cat(sprintf(info2), "\n")
-    })
-    
-    output$info.out <- renderPrint({
-        info()
-    })
-
-    
-    
-    output$textarea.out <- renderPrint({
-        bs()
-    })
-    
-    output$testnorm.out <- renderPrint({
-        testnorm()
-    })
-    
-    output$downloadDistPlot <- downloadHandler(
-    filename = function() {
-        paste('Distribution-', Sys.Date(), '.pdf', sep='')
-    },
-    content = function(FILE=NULL) {
-        pdf(file=FILE)
-		print(makedistPlot())
-		dev.off()
-	})
-    
-    output$downloadBoxPlot <- downloadHandler(
-    filename = function() {
-        paste('Boxplot-', Sys.Date(), '.pdf', sep='')
-    },
-    content = function(FILE=NULL) {
-        pdf(file=FILE)
-		print(makeboxPlot())
-		dev.off()
-	})
-    
-    output$downloadQQPlot <- downloadHandler(
-    filename = function() {
-        paste('QQplot-', Sys.Date(), '.pdf', sep='')
-    },
-    content = function(FILE=NULL) {
-        pdf(file=FILE)
-		print(makeqqPlot())
-		dev.off()
-	})
+    output$downloadData <- downloadHandler(
+        filename = function() {
+            paste('Data-', Sys.Date(), '.csv', sep='')
+        },
+        content = function(file) {
+            write.csv(check(), file)
+        }
+    )
 
 })
